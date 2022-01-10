72d29b43d8f6de076e9fc43a4e63be5cdab2c697
Holly Lui <hollylui@gmail.com>
2022-01-10 11:26:29 +0100

    contact form removed

--- src/views/contact/ContactContent.jsx
+++ src/views/contact/ContactContent.jsx
@@ -1,11 +1,11 @@
 import ContactEmail from "./ContactEmail";
-import ContactForm from "./ContactForm";
+// import ContactForm from "./ContactForm";
 
 const ContactContent = () => {
   return (
     <div className="contactContent">
       <ContactEmail />
-      <ContactForm />
+      {/* <ContactForm /> */}
     </div>
   );
 };
